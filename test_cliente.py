59312b0a8028e8e2235307055df55d3438241395
Fernando Hernandez <fr.nando.d@gmail.com>
2026-02-25 13:23:54 -0600

    refactor: usa fixtures para eliminar repeticion en pruebas

--- test_cliente.py
+++ test_cliente.py
@@ -1,16 +1,17 @@
 from cliente import Cliente
 import pytest
 
-def test_cliente_nuevo_lista_vacia():
-    cliente = Cliente("Fernando", 20)
+@pytest.fixture 
+def cliente():
+    return Cliente("Fernando", 20)
+
+def test_cliente_nuevo_lista_vacia(cliente):
     assert len(cliente.ventas) == 0
 
-def test_cliente_guardado_correctamente():
-    cliente = Cliente("Fernando", 20)
+def test_cliente_guardado_correctamente(cliente):
     assert cliente.nombre == "Fernando"
 
-def test_ventas_igual_0():
-    cliente = Cliente("Fernando", 29)
+def test_ventas_igual_0(cliente):
     assert cliente.total_consumido() == 0
 
 def test_edad_invalida():
