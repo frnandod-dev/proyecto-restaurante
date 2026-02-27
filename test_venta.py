f015b9162b282fb564eb086b93fc7fbc400bb355
Fernando Hernandez <fr.nando.d@gmail.com>
2026-02-27 12:58:54 -0600

    feat: agrega pruebas para clase Venta

---
+++ test_venta.py
@@ -0,0 +1,9 @@
+import pytest
+from venta import Venta 
+
+@pytest.fixture 
+def venta1():
+    return Venta("hamburguesa", 390, 56, 446)
+
+def test_total(venta1):
+    assert venta1.total == venta1.subtotal + venta1.iva
